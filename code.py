8696e7d612dcdcf82d3c4db0d7eec035e5c91067
Yunus Emre Aydemir <73905666+yunusemreaydemir01@users.noreply.github.com>
2023-02-05 23:56:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,3 +12,18 @@ def flatten(l):
     return flatten_list
 
 print(flatten(list1))
+
+"""
+EXAMPLE 2
+""""
+
+list2 = [[1, 2], [3, 4], [5, 6, 7]]  
+
+def reverse_list(l):
+    l.reverse()
+    for sublist in l:
+        if type(sublist) == list:
+            reverse_list(sublist)
+
+reverse_list(list2)
+print(list2)
